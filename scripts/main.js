605df9045898f3feae7b504af41abc6163671f93
newelhan <ne.welhan@gmail.com>
2024-08-13 19:36:38 +0700

    Added GSAP and sidebar tooltip when clicking the profile button

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,3 @@
-// Use JS to auto generate the SIDEBAR buttons cause i'm lazy
-sidebarButtons = ['home', 'explore', 'notifications', 'messages', 'grok', 'communities', 'profile', 'more']
-
-sidebarButtons.forEach(name => {
-  document.querySelector('.button-lists').innerHTML += `<div class="sidebar-buttons">
-        <button onclick="location.href='https://x.com/${name}'">
-          <img id="${name}-icon" src="img/x-${name}.png">
-          <span class="button-text">${name.charAt(0).toUpperCase() + name.slice(1)}</span>
-        </button>
-      </div>`
-})
-
 // Make boolean value to change the post data and when refreshed still the same?
 let forYou = 1
 
---
+++ scripts/sidebar.js
@@ -0,0 +1,34 @@
+// Use JS to auto generate the SIDEBAR buttons cause i'm lazy
+sidebarButtons = ['home', 'explore', 'notifications', 'messages', 'grok', 'communities', 'profile', 'more']
+
+sidebarButtons.forEach(name => {
+  document.querySelector('.button-lists').innerHTML += `<div class="sidebar-buttons">
+        <button onclick="location.href='https://x.com/${name}'">
+          <img id="${name}-icon" src="img/x-${name}.png">
+          <span class="button-text">${name.charAt(0).toUpperCase() + name.slice(1)}</span>
+        </button>
+      </div>`
+})
+
+// Tooltip for profile button
+document.addEventListener("DOMContentLoaded", function () {
+  const profileButton = document.getElementById("profile-button");
+  const tooltip = document.querySelector(".profile-button-tooltip");
+
+  profileButton.addEventListener("click", function (event) {
+    event.stopPropagation();
+    if (tooltip.style.opacity == 0 || tooltip.style.opacity === '') {
+      gsap.to(tooltip, { duration: 0.3, opacity: 1, visibility: 'visible', ease: "power2.inOut" });
+    } else {
+      gsap.to(tooltip, { duration: 0.3, opacity: 0, visibility: 'hidden', ease: "power2.inOut" });
+    }
+  });
+
+  document.addEventListener("click", function () {
+    gsap.to(tooltip, { duration: 0.3, opacity: 0, visibility: 'hidden', ease: "power2.inOut" });
+  });
+
+  tooltip.addEventListener("click", function (event) {
+    event.stopPropagation();
+  });
+});
